0e04a09fff5a9c8c7b7138e10f3f5b344bc76da6
John Lauer <jlauer12@gmail.com>
2020-06-02 11:46:54 -0700

    both

---
+++ tampermonkey.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Zipwhip Plugin Bootstrapper for App Store
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://app.zipwhip.com/*
+// @grant        none
+// ==/UserScript==
+
+console.log("Plugin - Pre-run");
+setTimeout(function() {
+
+    console.log("Plugin - Bootsrapper running...");
+
+    var script = document.createElement('script');
+    //script.setAttribute('src', 'https://raw.githubusercontent.com/chilipeppr/zw-plugin-bootstrap/master/boot.js');
+    //script.setAttribute('type', 'text/javascript');
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        var DONE = 4; // readyState 4 means the request is done.
+        var OK = 200; // status 200 is a successful return.
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+                console.log("Plugin - Got boot.js downloaded"); // 'This is the returned text.'
+                script.text = xhr.responseText;
+                document.getElementsByTagName('head')[0].appendChild(script);
+
+            } else {
+                console.log('Error: ' + xhr.status); // An error occurred during the request.
+            }
+        }
+    };
+
+    xhr.open('GET', 'https://raw.githubusercontent.com/chilipeppr/zw-plugin-bootstrap/master/boot.js');
+    xhr.send(null);
+
+    console.log("Plugin - Bootsrapper ran");
+}, 1000);
